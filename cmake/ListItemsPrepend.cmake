eab5e5c262b763b27f185df9dc616055da335000
Adel Haddad <26027314+adehad@users.noreply.github.com>
2024-01-01 13:13:59 +0000

    cleanse some cpp and move folders

--- cmake/ListItemsPrepend.cmake
+++
@@ -1,7 +0,0 @@
-function (list_items_prepend List Prefix)
-  set(${List}_temp)
-  foreach (_item ${${List}})
-    list(APPEND ${List}_temp "${Prefix}${_item}")
-  endforeach()
-  set(${List} "${${List}_temp}" PARENT_SCOPE)
-endfunction()
