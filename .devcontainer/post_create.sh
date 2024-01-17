9af446ed45148949975c4d5dfecc99375bc3c1f1
adehad <26027314+adehad@users.noreply.github.com>
2024-01-17 18:36:56 +0000

    use lighter Docker image

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
- git config --global --add safe.directory /workspaces/py-scantailor-advanced
+git config --global --add safe.directory /workspaces/py-scantailor-advanced
+
+# Install pipx
+python -m pip install pipx
+pipx ensurepath
+
+# Allow path changes
+. ~/.bashrc
 
 # Install pdm and pre-commit
 pipx install pre-commit
 pipx install pdm
 
 pdm install
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+echo -e "${RED}You may need to 'Reload Window' in VSCode for it to pick up the updated interpreter${NC}"
